c87ec2efab24fc8b59078dc7354bb8dfe623a309
fos <darkfos82@gmail.com>
2023-12-15 18:31:49 +0300

    Добавли в админ панель модель записей

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Модель таблицы Post в админке
+    """
+
+    #Показ определённых полей модели
+    list_display = ["title", "slug", "body", "publish_date", "status"]
+
+    #Фильтр поиска
+    list_filter = ["status", "created_date", "publish_date", "user_id"]
+
+    #Поисковик по полям
+    search_fields = ["title", "body"]
+
+    #Предопределение для поля, постановка записи из title
+    prepopulated_fields = {"slug": ("title",)}
+
+    #ID таблицы User
+    raw_id_fields = ["user_id"]
+
+    #Навигация - ссылки
+    date_hierarchy = "publish_date"
+
+    #Упорядочивание
+    ordering = ["status", "publish_date"]
