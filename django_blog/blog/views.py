9f951343dba53ad5b3b74bd8b8fc80acc17db638
fos <darkfos82@gmail.com>
2023-12-16 21:03:27 +0300

    Рефакторинг кода

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Post
 
@@ -13,8 +13,11 @@ def post_list(request):
     return render(request, "blog/post/list.html", context=all_posts)
 
 def post_detail(request, post_id: int):
-    try:
-        post = Post.published.get(id=post_id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found")
+    """
+    Возвращает информацию об 1 записи по его id
+    :param request:
+    :param post_id:
+    :return:
+    """
+    post = get_object_or_404(Post, id=post_id, status=Post.status.PUBLISHED)
     return render(request, "blog/post/detail.html", context=post)
