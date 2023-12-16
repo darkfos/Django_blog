887f40f55a0e37ec5ea55f590c7411120b25a4b8
fos <darkfos82@gmail.com>
2023-12-16 21:00:50 +0300

    Добавил 2 представления, представление всех записей и представление 1 записи

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    """
+    Выдаёт все записи из таблицы Post. С модификатором Published
+    :param request:
+    :return:
+    """
+    all_posts = Post.published.all()
+    return render(request, "blog/post/list.html", context=all_posts)
+
+def post_detail(request, post_id: int):
+    try:
+        post = Post.published.get(id=post_id)
+    except Post.DoesNotExist:
+        raise Http404("No Post found")
+    return render(request, "blog/post/detail.html", context=post)
