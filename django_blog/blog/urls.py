55184615839c82051c2d64a3489d64529bdebd24
fos <darkfos82@gmail.com>
2023-12-18 17:00:06 +0300

    Рефакторинг кода

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,5 +6,5 @@ app_name = "blog"
 #Представления поста
 urlpatterns = [
     path("", views.post_list, name='post_list'),
-    path("<int:post_id>", views.post_detail, name="post_detail")
+    path("<int:year>/<int:month>/<int:day>/<slug:post>/", views.post_detail, name="post_detail")
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -12,12 +12,12 @@ def post_list(request):
     all_posts = Post.objects.all()
     return render(request, "blog/post/list.html", {"posts": all_posts})
 
-def post_detail(request, post_id: int):
+def post_detail(request, year, month, day, post):
     """
     Возвращает информацию об 1 записи по его id
     :param request:
     :param post_id:
     :return:
     """
-    post = get_object_or_404(Post, id=post_id)
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post, publish_date__year=year, publish_date__month=month, publish_date__day=day)
     return render(request, "blog/post/detail.html", {"post": post})
